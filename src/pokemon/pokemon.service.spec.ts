0181fc21afc387404484421f695764b8c729abd2
Seunghyun Kim <kshlove735@suhwooms.com>
2023-07-07 20:51:43 +0900

    test by mocking on the Http dependency

--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -1,19 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonService } from './pokemon.service';
-import { HttpModule } from '@nestjs/axios';
-import { BadRequestException } from '@nestjs/common';
-import exp from 'constants';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 
 describe('PokemonService', () => {
   let pokemonService: PokemonService; // renamed variable to pokemonService
+  // DeepMocked<HttpService> 타입은 모든 properties와  하위 properties를 자동 완성하게 해준다.
+  let httpService: DeepMocked<HttpService>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule],  // import HttpModule
-      providers: [PokemonService],
+      providers: [PokemonService,
+        // set provide
+        {
+          provide: HttpService,
+          useValue: createMock<HttpService>(),
+        },
+      ],
     }).compile();
 
     pokemonService = module.get<PokemonService>(PokemonService);
+    httpService = module.get(HttpService);
   });
 
   it('should be defined', () => {
@@ -34,10 +42,34 @@ describe('PokemonService', () => {
       await expect(getPokemon).rejects.toBeInstanceOf(BadRequestException);
     })
 
-    // 3. api와 호출한 data가 예상과 다르면 return a InternalServerErrorException
+    // 3. api와 호출한 data가 예상한 양식으로 온다면 return data.species.name
     it('valid pokemon ID to return the pokemon name', async () => {
+      httpService.axiosRef.mockResolvedValueOnce({
+        data: {
+          species: { name: 'bulbasaur' }
+        },
+        headers: {},
+        config: { url: '' },
+        status: 200,
+        statusText: '',
+      })
       const getPokemon = pokemonService.getPokemon(1);
       await expect(getPokemon).resolves.toBe('bulbasaur')
+    })
+
+    // 3. api와 호출한 data가 예상과 다르면 return a InternalServerErrorException
+    it('if Pokemon API response unexpectedly changes, throw an error', async () => {
+      httpService.axiosRef.mockResolvedValueOnce({
+        data: 'Unexpected data',
+        headers: {},
+        config: { url: '' },
+        status: 200,
+        statusText: '',
+      })
+
+      const getPokemon = pokemonService.getPokemon(1);
+
+      await expect(getPokemon).rejects.toBeInstanceOf(InternalServerErrorException);
     })
   })
 
